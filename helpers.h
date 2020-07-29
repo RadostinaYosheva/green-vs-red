6cf1fa2c488ff2b87918eef4699157b8f8f7bd32
RadostinaYosheva <r.yosheva@gmail.com>
2020-07-29 21:51:35 +0300

    Add input for coordinates

--- helpers.h
+++ helpers.h
@@ -18,14 +18,27 @@ bool areValidCoordinates (int width, int height, int targetX, int targetY){
 
 void inputWidthAndHeight(int width, int height){
     // Message for input
-    std::cout << "Enter width and height: ";
+    std::cout << "Enter width and height : ";
     std::cin >> width >> height;
     std::cout << std::endl;
 
-    while (areValidGridBorders(width, height)){
-        std::cout << "Error: Invalid values of width or/and height (0 < x <= y < 1000)\n" << std::endl;
-        std::cout << "Enter width and height: ";
+    while (!areValidGridBorders(width, height)){
+        std::cout << "Error: Invalid values of width or/and height (0 < x <= y < 1000).\n" << std::endl;
+        std::cout << "Enter width and height : ";
         std::cin >> width >> height;
+        std::cout << std::endl;
+    }
+}
+
+void inputCoordinates(int width, int height, int targetX, int targetY){
+    // Message for input
+    std::cout << "\nEnter target cell's coordinates : ";
+    std::cin >> targetX >> targetY;
+
+    while (!areValidCoordinates(width, height, targetX, targetY)) {
+        std::cout << "Error: Invalid coordinates.\n" << std::endl;
+        std::cout << "Enter target cell's coordinates : ";
+        std::cin >> targetX >> targetY;
         std::cout << std::endl;
     }
 }
